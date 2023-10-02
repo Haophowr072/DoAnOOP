f70ba4ca5b934f256057c5647052d6bab82be2bc
Thanh Huy <thanhhuy280600@gmail.com>
2023-10-02 15:52:38 +0700

    update

--- src/main/java/com/mycompany/doanoop/DoAnOOP.java
+++ src/main/java/com/mycompany/doanoop/DoAnOOP.java
@@ -11,6 +11,9 @@ package com.mycompany.doanoop;
 public class DoAnOOP {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        PhuongTien phuongtien = new PhuongTien();
+        phuongtien.nhap();
+        phuongtien.xuat();
+        
     }
 }
