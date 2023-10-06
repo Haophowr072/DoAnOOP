ae571a72e4a29dbebb3dcc9b25c66e0f4b291a12
Y Hao Ne <nhathao1290@gmail.com>
2023-10-06 09:13:50 +0700

    update

--- src/main/java/com/mycompany/doanoop/TourDuLich.java
+++ src/main/java/com/mycompany/doanoop/TourDuLich.java
@@ -1,2 +1,9 @@
-package com.mycompany.doanoop;public class TourDuLich {
+package com.mycompany.doanoop;
+
+public class TourDuLich {
+    private String tenTour;
+    private String ngayDi;
+    private String ngayVe;
+    private int giaTien;
+    private DanhSachThanhVien dstv;
 }
