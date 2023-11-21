5e9e160a8dff8e32725dfd631b511b542dca44ed
Hào Phở <nhathao1290@gmail.com>
2023-11-21 10:34:12 +0700

    main

--- src/main/java/com/mycompany/doanoop/Main.java
+++ src/main/java/com/mycompany/doanoop/Main.java
@@ -8,49 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-       /* ConNguoi[] cn = new ConNguoi[3];
-
-
-        
-        PhuongTien[] phuongtien = new PhuongTien[5];
-        phuongtien[0]= new PhuongTien("99999", 4 ,"xe hơi");
-        phuongtien[1]= new PhuongTien("77777", 2 , "xe máy" );
-        phuongtien[2]= new PhuongTien("55555", 2 , "xe máy" );
-        phuongtien[3]= new PhuongTien("66666", 2 , "xe máy" );
-        phuongtien[4]= new PhuongTien("11111", 2 , "xe máy" );
-        DanhSachPhuongTien danhsach = new DanhSachPhuongTien(phuongtien , phuongtien.length);  
-        DanhSachThanhPho pho = new DanhSachThanhPho(thanhpho , thanhpho.length);
-        pho.showMenu();
-
-        ConNguoi[] cn = new ConNguoi[3];
-
-        cn[0] = new HanhKhach("Nguyễn Nhật Hào", "7/2/2002", "Bình Dương", "0123456789", "3336665544", "VIP", "30");
-        cn[1] = new HanhKhach("Triệu Võ Thanh Huy", "7/2/2002", "Hồ Chí Minh", "365645854", "778899554", "VIP", "30");
-        cn[2] = new HuongDanVien("Nguyễn Anh Duy", "7/2/2002", "Hồ Chí Minh", "365645854", "778899554", "2 năm", "20/10/2023");
-        DanhSachThanhVien dstv = new DanhSachThanhVien(cn, cn.length);
-        dstv.showMenu();*/
-
-//        PhuongTien[] phuongtien = new PhuongTien[2];
-//        phuongtien[0]= new PhuongTien("99999", 4 ,"xe hoi");
-//        phuongtien[1]= new PhuongTien("77777", 2 , "xe may" );
-//        DanhSachPhuongTien danhsach = new DanhSachPhuongTien(phuongtien , phuongtien.length);
-
-//
-//
-//        ThanhPho[] thanhpho = new ThanhPho[2];
-//        thanhpho[0]=new ThanhPho("Hồ Chí Minh" , "Nóng", "28" ,"Tiếng Việt", " 2/3/2023" , "12/3/2023");
-//        thanhpho[1]=new ThanhPho("Hà Nội" , "Nóng", "29" ,"Tiếng Việt", "5/6/2023" , "6/6/2023");
-//        DanhSachThanhPho pho = new DanhSachThanhPho(thanhpho , thanhpho.length);
-//        pho.them();
-//        pho.xuat();
-    	
-//
-    	CoSoKinhDoanh[] cskd=new CoSoKinhDoanh[2];
-    	cskd[0]= new CoSoKinhDoanh("KS1"," Ngoi Sao","172 ADV","5");
-    	cskd[1]= new CoSoKinhDoanh("NH1"," Phuong Nam","41 PN","6");
-    	DanhSachCoSoKinhDoanh dscskd=new DanhSachCoSoKinhDoanh(cskd,cskd.length);
-
-    	dscskd.showMenu();
-
+       
     }
 }
